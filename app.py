e575483f1c355d69e74b4e95d018faa0bc91c195
Kiarash <kiamhn1998@gmail.com>
2018-11-14 14:47:17 +0330

    feat : Add some test cars

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask_migrate import Migrate, MigrateCommand
 from QP import app, db
 from QP.auth.models import User
+from QP.car.models import Car
 from flask_script import Manager, prompt_bool
 manager = Manager(app)
 migrate = Migrate(app, db)
@@ -12,6 +13,17 @@ def initdb():
     db.create_all()
     user = User(name="admin", username="admin", password="admin", role="admin")
     db.session.add(user)
+    db.session.commit()
+    car = Car(name="peykan", factory="bmw", kilometer=0, year=1931, color="sefid yakhchali",
+              description="mashin e khafan", automate=True, user_id=1, price=1000, image_url="peykan.jpg")
+    carr = Car(name="peykan", factory="bmw", kilometer=0, year=1931, color="sefid yakhchali",
+               description="mashin e khafan", automate=True, user_id=1, price=1000, image_url="peykan.jpg")
+    carrr = Car(name="peykan", factory="bmw", kilometer=0, year=1931, color="sefid yakhchali",
+                description="mashin e khafan", automate=True, user_id=1, price=1000, image_url="peykan.jpg")
+    db.session.add(car)
+    db.session.add(carr)
+    db.session.add(carrr)
+
     db.session.commit()
     print('Initialized the database')
 
