c807dc0b98445e26f06a07f9b47c03732ed24f14
Kiarash <kiamhn1998@gmail.com>
2018-11-14 13:00:42 +0330

    feat : Add car module and Car model class
    - Add car module and Car class in /car/models.py

---
+++ QP/car/models.py
@@ -0,0 +1,39 @@
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+from QP import db
+from QP.auth.models import User
+
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    factory = db.Column(db.String(20))
+    kilometer = db.Column(db.Integer)
+    year = db.Column(db.Integer)
+    color = db.Column(db.String(20))
+    description = db.Column(db.String(100))
+    automate = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    price = db.Column(db.Integer)
+    image_url = db.Column(db.String)
+
+    def __repr__(self):
+        return "<Car '{}'>".format(self.name)
+
+    @staticmethod
+    def get_by_owner(user_id):
+        return User.query.filter_by(user_id=user_id)
+
+    def serialize(self):
+        return {
+            'name' : self.name,
+            'factory' : self.factory,
+            'kilometer' : self.kilometer,
+            'year' : self.year,
+            'color' : self.color,
+            'description' : self.description,
+            'automate' : self.automate,
+            'user_id' : self.user_id,
+            'price' : self.price,
+            'id' : self.id
+        }
