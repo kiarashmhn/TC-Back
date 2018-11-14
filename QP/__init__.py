8d73a36c97d42b38d26e38cf7139bbe1716a79f9
Kiarash <kiamhn1998@gmail.com>
2018-11-14 12:03:39 +0330

    feat : Register auth blueprint in QP/__init__.py

--- QP/__init__.py
+++ QP/__init__.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, request, jsonify, redirect, render_template, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+
+from QP.auth.controllers import auth
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '~t\x86\xc9\x1ew\x8bOcX\x85O\xb6\xa2\x11kL\xd1\xce\x7f\x14<y\x9e'
@@ -8,3 +11,5 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'th
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+app.register_blueprint(auth, url_prefix='/auth')
