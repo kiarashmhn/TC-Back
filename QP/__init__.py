d6659d9a25dec10ed18519170b0a77c57a3b3bfc
Kiarash <kiamhn1998@gmail.com>
2018-11-14 11:01:15 +0330

    feat : Add project
    - Add app.py and __init__.py
    - Add requirements.txt
    - Add .gitignore
    - Add flask_script Manager in app.py
    - Add auth module

---
+++ QP/__init__.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask, request, jsonify, redirect, render_template, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '~t\x86\xc9\x1ew\x8bOcX\x85O\xb6\xa2\x11kL\xd1\xce\x7f\x14<y\x9e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
