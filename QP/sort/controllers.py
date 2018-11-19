3066d37304f6fc3f23ecf9b8c57eef494fe589c4
Kiarash <kiamhn1998@gmail.com>
2018-11-19 13:29:52 +0330

    feat : Add sort api

--- QP/sort/controllers.py
+++ QP/sort/controllers.py
@@ -9,6 +9,7 @@ from QP.car.models import Car
 from flask_login import login_required, login_user, logout_user, current_user
 from QP import ResponseObject
 from flask import Blueprint
+from sqlalchemy import desc
 
 
 srt = Blueprint('sort', __name__)
@@ -17,3 +18,32 @@ srt = Blueprint('sort', __name__)
 class SortHandler():
     def __init__(self):
         pass
+
+    @staticmethod
+    @srt.route('cars/sort/<field>/<int:ascending>', methods=["GET"])
+    @login_required
+    def list_car(field, ascending):
+        if field == 'year':
+            if ascending == 1:
+                cars = Car.query.filter(Car.year.isnot(None)).order_by(Car.year).all()
+            elif ascending == 0:
+                cars = Car.query.filter(Car.year.isnot(None)).order_by(desc(Car.year)).all()
+            else:
+                response = ResponseObject.ResponseObject(obj=None, status='wrong input!')
+                return jsonify(response.serialize())
+        elif field == 'price':
+            if ascending == 1:
+                cars = Car.query.filter(Car.price.isnot(None)).order_by(Car.price).all()
+            elif ascending == 0:
+                cars = Car.query.filter(Car.price.isnot(None)).order_by(desc(Car.price)).all()
+            else:
+                response = ResponseObject.ResponseObject(obj=None, status='wrong input!')
+                return jsonify(response.serialize())
+        else:
+            response = ResponseObject.ResponseObject(obj=None, status='wrong field!')
+            return jsonify(response.serialize())
+        if cars is None:
+            response = ResponseObject.ResponseObject(obj=None, status='no cars in the database!')
+            return jsonify(response.serialize())
+        response = ResponseObject.ResponseObject(obj=cars, status='OK')
+        return jsonify(response.serialize())
