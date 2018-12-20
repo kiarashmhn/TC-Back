16672b14d2e995b3f50d6dcb24a52cd77c2ccde4
Kiarash <kiamhn1998@gmail.com>
2018-12-20 22:58:56 +0330

    feat : register rent blueprint

--- QP/__init__.py
+++ QP/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
+app = Flask(__name__, static_folder='/Users/kiarash/Desktop/TC-Back')
 Swagger(app)
 app.config['SECRET_KEY'] = '~t\x86\xc9\x1ew\x8bOcX\x85O\xb6\xa2\x11kL\xd1\xce\x7f\x14<y\x9e'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
@@ -25,6 +25,7 @@ from QP.car.controllers import car
 from QP.car.models import Car
 from QP.sort.controllers import srt
 from QP.image.controllers import img
+from QP.rent.controllers import rnt
 
 base_url = '/api/v1'
 
@@ -32,3 +33,4 @@ app.register_blueprint(usr, url_prefix=base_url + '/users')
 app.register_blueprint(car, url_prefix=base_url+'/cars')
 app.register_blueprint(srt, url_prefix=base_url+'/sort')
 app.register_blueprint(img, url_prefix=base_url+'/images')
+app.register_blueprint(rnt, url_prefix=base_url+'/rent')
