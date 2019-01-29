8a4d48757e0756d8f849eb475c16532664b2f07d
Kiarash <kiamhn1998@gmail.com>
2019-01-29 15:09:44 +0330

    feat : Add SearchHandler class and add_to_index method

---
+++ QP/search/search.py
@@ -0,0 +1,14 @@
+from flask import current_app
+
+
+class SearchHandler():
+
+    @staticmethod
+    def add_to_index(index, model):
+        if not current_app.elasticsearch:
+            return
+        payload = {}
+        for field in model.__searchable__:
+            payload[field] = getattr(model, field)
+        current_app.elasticsearch.index(index=index, doc_type=index, id=model.id,
+                                        body=payload)
