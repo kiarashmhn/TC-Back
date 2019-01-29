5cf82219c43f329168e462b52420c87a62edc591
Kiarash <kiamhn1998@gmail.com>
2019-01-29 15:13:20 +0330

    feat : Add search method to SearchableMixin

--- QP/search/models.py
+++ QP/search/models.py
@@ -3,6 +3,16 @@ from QP import app,db
 
 
 class SearchableMixin(object):
+    @classmethod
+    def search(cls, expression, page, per_page):
+        ids, total = SearchHandler.query_index(cls.__tablename__, expression, page, per_page)
+        if total == 0:
+            return cls.query.filter_by(id=0), 0
+        when = []
+        for i in range(len(ids)):
+            when.append((ids[i], i))
+        return cls.query.filter(cls.id.in_(ids)).order_by(
+            db.case(when, value=cls.id)), total
 
     @classmethod
     def before_commit(cls, session):
