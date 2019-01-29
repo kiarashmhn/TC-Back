7324a8631cbf21eed93a887fbc4e4b8ac9c98468
Kiarash <kiamhn1998@gmail.com>
2019-01-29 15:11:04 +0330

    feat : Add query_index method

--- QP/search/search.py
+++ QP/search/search.py
@@ -18,3 +18,15 @@ class SearchHandler():
         if not current_app.elasticsearch:
             return
         current_app.elasticsearch.delete(index=index, doc_type=index, id=model.id)
+
+    @staticmethod
+    def query_index(index, query, page, per_page):
+        if not current_app.elasticsearch:
+            return [], 0
+        search = current_app.elasticsearch.search(
+            index=index, doc_type=index,
+            body={'query': {'multi_match': {'query': query, 'fields': ['name', 'factory', 'color']}}})
+        ids = [int(hit['_id']) for hit in search['hits']['hits']]
+        scores = [int(hit['_score']) for hit in search['hits']['hits']]
+        print(scores)
+        return ids, search['hits']['total']
